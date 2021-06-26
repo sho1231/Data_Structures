f494db9f49ae2c0128057fc3c4f07902e581d427
sho1231 <38104743+sho1231@users.noreply.github.com>
2021-06-26 11:26:06 +0530

    Update main.c

--- LinkedList/Single_Linked List Menu driven program/main.c
+++ LinkedList/Single_Linked List Menu driven program/main.c
@@ -2,258 +2,257 @@
 #include <stdlib.h>
 struct node
 {
-    int data;
+    int d;
     struct node *adr;
-
 };
 struct node *head,*newnode,*temp;
 int count=0;
-void in_b()
+void insert_b()
 {
     if(head==0)
-    {   newnode->adr=0;
+    {
         head=temp=newnode;
+        newnode->adr=head;
     }
     else
     {
-       newnode->adr=head;
-       head=newnode;
+        newnode->adr=head;
+        head=newnode;
     }
 }
- void in_e()
-    { newnode->adr=0;
-        if(head==0)
-            head=temp=newnode;
-        else
-        {
-            temp->adr=newnode;
-            temp=newnode;
-
-        }
-    }
- void in_m(int pos)
- {
-     struct node *t,*a;
-     int trav=1;
-     t=head;
-     while(t!=0)
-    {
-        if(trav==(pos-1))
-     {
-         a=t->adr;
-         t->adr=newnode;
-         break;
-     }
-     t=t->adr;
-     trav++;
-
-    }
-    newnode->adr=a;
-
- }
- void del_b()
- {
-     struct node *t=head;
-     head=t->adr;
-     free(t);
- }
- void de_a(int pos)
+void del_b()
 {
-    int trav=1;
-    struct node *t,*a,*b;
-    t=head;
-    while(t!=0)
+    if(count<2)
     {
-        if(trav==(pos-1))
-        {
+        free(head);
+        head=0;
+    }
+    else
+    {
+        struct node *t;
+        t=head->adr;
+        free(head);
+        head=t;
+    }
+}
+void del_e()
+{
+    struct node *t,*a;
+    int trav=1;
+    t=head;
+    while(trav<(count+1))
+    {
+        if(trav==count-1)
+        {  // printf("%d\n",t->d);
+            temp=t;
             a=t->adr;
-            b=a->adr;
-            t->adr=b;
+            t->adr=head;
             free(a);
             break;
-
         }
+        t=t->adr;
         trav++;
-        t=t->adr;
     }
 }
-
- void del_e()
- {
-     struct node *t;
-     int trav=1;
-     t=head;
-     while(t!=0)
-     {
-         if(trav==(count-1))
-         {
-             free(t->adr);
-             t->adr=0;
-             break;
-         }
-
-         trav++;
-         t=t->adr;
-
-     }
- }
-void d_o()
-{
-    int odd=1;
-    struct node *t;
+void del_a(int pos)
+{ //printf("%d\n",count);
+    struct node *t,*a,*b;int trav=1;
     t=head;
-    while(t!=0)
+    while(trav!=(count+1))
     {
-        if(odd%2!=0)
-            printf("%d\n",t->data);
-        t=t->adr;
-        odd++;
-    }
-}
-void d_e()
-{
-    int even=1;
-    struct node *t;
-    t=head;
-    while(t!=0)
-    {   if(even%2==0)
+        if(trav==pos-1)
         {
-        printf("%d\n",t->data);
+            a=t;
+           // printf("%d\n",a->d);
         }
-        t=t->adr;
-        even++;
-    }
-}
-void rev()
-{
-    struct node*t,*a,*b;//t=address of next node,a=address of next node,b=address of prev node
-    int trav=1;
-    t=head;
-    while(t!=0)
-    {
-        if(trav==1)
+        else if(trav==pos)
         {
-            a=t->adr;
-            t->adr=0;
-            b=t;
-            t=a;
-        }
-        else if(trav<count)
-        {
-            a=t->adr;
-            t->adr=b;
-            b=t;
-            t=a;
-        }
-        else if(trav==count)
-        {
-            t->adr=b;
-            head=t;
+            b=t->adr;
+           // printf("%d\n",b->d);
+            a->adr=b;
+            //printf("%d\n",a->d);
+            free(t);
+            //printf("%d\n",count);
             break;
         }
+        t=t->adr;
         trav++;
     }
+
+}
+void insert_e()
+{
+    newnode->adr=head;
+    if(head==0)
+        head=temp=newnode;
+    else
+    {
+        temp->adr=newnode;
+        //printf("%d\n",temp->d);
+        temp=newnode;
+    }
+}
+void insert_a(int pos)
+{
+    struct node *t,*a;int trav=1;
+    t=head;
+    while(trav<(count+1))
+    {
+        if(trav==pos-1)
+        {
+            a=t->adr;
+           // printf("%d\n",t->adr);//next old node
+            t->adr=newnode;
+            //printf("%d\n",t->adr);//next newnode
+            break;
+        }
+        t=t->adr;
+        trav++;
+    }
+    newnode->adr=a;//next old node
 
 }
 void swap(int pos1,int pos2)
 {
-    struct node *t,*a;int trav=1,t1,t2;
+    struct node *t,*t1;int a,trav=1;
     t=head;
-    while(t!=0)
+    while(trav!=(count+1))
     {
         if(trav==pos1)
         {
-            a=t;
-            t1=t->data;
+            a=t->d;
+            t1=t;
         }
         else if(trav==pos2)
         {
-            t2=t->data;
-            t->data=t1;
-            a->data=t2;
+            t1->d=t->d;
+            t->d=a;
             break;
         }
-        trav++;
         t=t->adr;
+        trav++;
     }
 }
-
-void display()
-{ struct node *t;
+void dis_e()
+{
+    struct node *t;int trav=1;
     t=head;
-    while(t!=0)
+    while(trav!=(count+1))
     {
-        printf("%d\n",t->data);
+        if(trav%2==0)
+            printf("%d\n",t->d);
         t=t->adr;
+        trav++;
+    }
+}
+void dis_o()
+{
+    struct node *t;int trav=1;
+    t=head;
+    while(trav!=(count+1))
+    {
+        if(trav%2!=0)
+            printf("%d\n",t->d);
+        t=t->adr;
+        trav++;
+    }
+}
+void rev()
+{
+    struct node *t,*a,*b;int trav=1;
+    t=head;
+    while(trav!=(count+1))
+    {
+        if(trav==1)
+        {
+            a=t->adr;
+            b=t;
+            head=temp;
+            temp=t;
+            t->adr=head;
+        }
+        else
+        {
+            a=t->adr;
+            t->adr=b;
+            b=t;
+        }
+        t=a;
+        trav++;
+    }
+}
+void display()
+{
+    struct node *t;int trav=count;
+    t=head;
+    while(trav!=0)
+    {
+        printf("%d\n",t->d);
+        t=t->adr;
+        trav--;
     }
 }
 int main()
-{ head=0;
-    int choice=1;
-    while(choice==1)
+{   head=0;
+    int ch=1;
+    while(ch==1)
     {
-        printf("1)Insert in the begining.\n2)Insert in the end.\n3)Insert in any position.\n4)Delete at the beginning.\n5)Delete at any position.\n6)Delete at the end.\n7)Display even nodes.\n8)Display odd nodes.\n9)Reversing the available nodes.\n10)Display nodes.\n11)Display total number of nodes.\n12)Swapping nodes.\n13)Exit.\n14)Clear the console.\n\n\n");
+        printf("1)Insert at the beginning.\n2)Insert at the end.\n3)Insert at any position.\n4)Delete at the beginning.\n5)Delete at the end.\n6)Delete at any position.\n7)Display even nodes.\n8)Display odd nodes.\n9)Reverse the nodes.\n10)Swapping the nodes.\n11)Display the nodes.\n12)Display the total number of nodes.\n13)Clear the console.\n14)Exit.\n\n\n");
         int c,pos,pos2;
         printf("Enter choice: ");
         scanf("%d",&c);
-        //int n;
         switch(c)
         {
         case 1:
-             newnode=(struct node*)malloc(sizeof(struct node));
-             count++;
-             printf("Enter data: ");
-             scanf("%d",&newnode->data);
-             in_b();
-             break;
+            newnode=(struct node*)malloc(sizeof(struct node));
+            count++;
+            printf("Enter data: ");
+            scanf("%d",&newnode->d);
+            insert_b();
+            break;
         case 2:
-                newnode=(struct node*)malloc(sizeof(struct node));
-                count++;
-                printf("Enter data:");
-                scanf("%d",&newnode->data);
-                in_e();
-                break;
+            newnode=(struct node*)malloc(sizeof(struct node));
+            count++;
+            printf("Enter data: ");
+            scanf("%d",&newnode->d);
+            insert_e();
+            break;
         case 3:
-           printf("Enter position: ");
-           scanf("%d",&pos);
+            printf("Enter position: ");
+            scanf("%d",&pos);
            while(pos>(count+1) || pos <1)
            {
                printf("Invalid position..enter correct position: ");
                scanf("%d",&pos);
            }
+           newnode=(struct node*)malloc(sizeof(struct node));
+           //printf("%d\n",newnode);
+           printf("Enter data:");
+           scanf("%d",&newnode->d);
            if(pos==1)
            {
-             newnode=(struct node*)malloc(sizeof(struct node));
-             count++;
-             printf("Enter data: ");
-             scanf("%d",&newnode->data);
-             in_b();
+            insert_b();
+            count++;
            }
            else if(pos==(count+1))
-           {
-               newnode=(struct node*)malloc(sizeof(struct node));
-               count++;
-               printf("Enter data: ");
-               scanf("%d",&newnode->data);
-               in_e();
+            {
+                insert_e();
+                count++;
            }
            else
            {
-            newnode=(struct node*)malloc(sizeof(struct node));
-            count++;
-             printf("Enter data: ");
-             scanf("%d",&newnode->data);
-               in_m(pos);
+                insert_a(pos);
+                count++;
            }
-           break;
+            break;
         case 4:
             if(head==0)
                 printf("Empty list..\n");
             else
             {
                 del_b();
-                printf("Node deleted..\n");
                 count--;
+                printf("Node deleted..\n");
             }
             break;
         case 5:
@@ -261,136 +260,114 @@ int main()
                 printf("Empty list..\n");
             else
             {
-                printf("Enter position: ");
-                scanf("%d",&pos);
-              while(pos>count|| pos <1)
-           {
-               printf("Invalid position..enter correct position: ");
-               scanf("%d",&pos);
-           }
-
-            if(pos==1)
-                if(head==0)
-                    printf("Empty list..\n");
-            else
+                if(count<2)
                 {
-                 del_b();
-                 count--;
+                    del_b();
+                    count--;
+                    printf("Node deleted..\n");
                 }
-            else if(pos==count)
-            {
-                if(head==0)
-                printf("Empty list..\n");
-            else
+                else
                 {
                     del_e();
                     count--;
+                    printf("Node deleted..\n");
                 }
             }
-            else
-            {
-                de_a(pos);
-                printf("Node %d deleted..\n",pos);
-                count--;
-            }
-
-            }
             break;
-
         case 6:
             if(head==0)
                 printf("Empty list..\n");
-            else if(count==1)
-            {
-                 del_b();
-                printf("Node deleted..\n");
-                count--;
-            }
-
             else
             {
-                del_e();
-                printf("Node deleted..\n");
+                printf("Enter pos: ");
+                scanf("%d",&pos);
+                while(pos>count || pos <1)
+                {
+                    printf("Invalid position...enter correct position: ");
+                    scanf("%d",&pos);
+                }
+                if(pos==1)
+                    del_b();
+                else if(pos==count)
+                    del_e();
+                else
+                    del_a(pos);
                 count--;
+                printf("Node deleted..\n");
             }
             break;
         case 7:
-             if(head==0)
+            if(head==0)
                 printf("Empty list..\n");
-            else if(count<2)
-            {
-                printf("No even nodes..\n");
-            }
             else
-                {
-                    printf("Displaying even nodes..\n");
-                    d_e();
-                }
+            {
+                printf("The even nodes are:\n");
+                dis_e();
+            }
             break;
         case 8:
-             if(head==0)
+            if(head==0)
                 printf("Empty list..\n");
             else
-                {
-                    printf("Displaying odd nodes..\n");
-                    d_o();
-                }
+            {
+                printf("The odd nodes are:\n");
+                dis_o();
+            }
             break;
         case 9:
             if(head==0)
                 printf("Empty list..\n");
             else
             {
-                if(count==1)
-                    printf("Only 1 node is avaliable..no reverse operation can be done..\n");
-                else
-                {
-                    rev();
-                    printf("Nodes reversed..\n");
-                }
+                rev();
+                printf("The nodes are reversed..\n");
             }
             break;
 
-        case 10:
+       case 10:
+            if(head==0)
+                printf("Empty list..\n");
+            else if(count<2)
+                printf("Swapping cannot occur when less than 2 nodes are present..\n");
+            else
+            {
+                printf("Enter the positions in ascending order: ");
+                scanf("%d%d",&pos,&pos2);
+                while(pos<1 || pos2<1 || pos>count || pos2 >count || pos>pos2)
+                {
+                    printf("One of the positions or both the positions might be invalid...please enter the correct positions in ascending order: ");
+                    scanf("%d%d",&pos,&pos2);
+                }
+                swap(pos,pos2);
+                printf("Nodes are swapped..\n");
+            }
+            break;
+        case 11:
             if(head==0)
                 printf("Empty list..\n");
             else
-                {
-                    printf("Displaying nodes..\n");
-                    display();
-                }
-            break;
-        case 11:
-            printf("Total number of nodes are: %d\n",count);
-            break;
-        case 12:
-            if(head==0 || count<2)
-                printf("Swapping can only occur when more than 1 node is present..\n");
-            else
             {
-                 printf("Enter the two positions: ");
-                scanf("%d%d",&pos,&pos2);
-                while(pos<1 || pos2<1 || pos>count || pos2>count || pos>pos2)
-                {
-                printf("The positions you entered is invalid or not in ascending order,please enter correct positions in ascending order: ");
-               scanf("%d%d",&pos,&pos2);
-                }
-                swap(pos,pos2);
-                printf("Nodes swapped..\n");
+                printf("Displaying nodes...\n");
+                display();
             }
             break;
+        case 12:
+            if(head==0)
+                printf("Empty list..\n");
+            else
+                printf("Total number of nodes: %d\n",count);
+            break;
         case 13:
-            printf("Thank you..\n\n");
-            choice=0;
+            system("cls");
             break;
         case 14:
-            system("cls");
+            printf("Thank you..\n");
+            ch=0;
             break;
         default:
             printf("Wrong choice..\n");
             break;
         }
     }
-
     return 0;
 }
